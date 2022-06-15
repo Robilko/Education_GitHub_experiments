d399b6ce3a40ecba01ed9c1ce32accb21da171bd
Robilko <sasha.robson@gmail.com>
2022-06-15 11:14:14 +0300

    Урок3. Пункт 1. Добавлены методы onAttach и onDetach в интерфейс PresenterContract. Теперь все Презентеры учитывают жизненный цикл View, который они хранят

--- app/src/main/java/com/example/githubtests/presenter/PresenterContract.kt
+++ app/src/main/java/com/example/githubtests/presenter/PresenterContract.kt
@@ -1,3 +1,8 @@
 package com.example.githubtests.presenter
 
-internal interface PresenterContract {}
+import com.example.githubtests.view.ViewContract
+
+internal interface PresenterContract {
+    fun onAttach(view: ViewContract)
+    fun onDetach()
+}
--- app/src/main/java/com/example/githubtests/presenter/details/DetailsPresenter.kt
+++ app/src/main/java/com/example/githubtests/presenter/details/DetailsPresenter.kt
@@ -1,23 +1,35 @@
 package com.example.githubtests.presenter.details
 
+import com.example.githubtests.view.ViewContract
 import com.example.githubtests.view.details.ViewDetailsContract
 
 internal class DetailsPresenter internal constructor(
-    private val viewContract: ViewDetailsContract,
     private var count: Int = 0
 ) : PresenterDetailsContract {
+
+    private var viewContract: ViewDetailsContract? = null
 
     override fun setCounter(count: Int) {
         this.count = count
     }
 
+    override fun getCounter(): Int = count
+
     override fun onIncrement() {
         count++
-        viewContract.setCount(count)
+        viewContract?.setCount(count)
     }
 
     override fun onDecrement() {
         count--
-        viewContract.setCount(count)
+        viewContract?.setCount(count)
+    }
+
+    override fun onAttach(view: ViewContract) {
+        viewContract = view as ViewDetailsContract
+    }
+
+    override fun onDetach() {
+        viewContract = null
     }
 }
