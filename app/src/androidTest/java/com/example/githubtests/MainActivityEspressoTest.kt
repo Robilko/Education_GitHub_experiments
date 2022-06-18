078db9521d481b19c7ad73edcf5fe9d01f7f5f55
Robilko <sasha.robson@gmail.com>
2022-06-19 00:37:21 +0300

    Урок4. Пункт 1. Покрыл тестами MainActivity (кроме элементов RecyclerView): убедился, что все элементы видны и корректно отображаются, что надписи на кнопке и в EditText верные и т. д.;

--- app/src/androidTest/java/com/example/githubtests/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/example/githubtests/MainActivityEspressoTest.kt
@@ -1,6 +1,11 @@
 package com.example.githubtests
 
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
@@ -10,6 +15,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.githubtests.view.search.MainActivity
+import junit.framework.TestCase
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
@@ -26,6 +32,79 @@ class MainActivityEspressoTest {
         scenario = ActivityScenario.launch(MainActivity::class.java)
     }
 
+    @Test //проверка на существование активити
+    fun activity_AssertNotNull() {
+        scenario.onActivity {
+            TestCase.assertNotNull(it)
+        }
+    }
+
+    @Test //Проверка на прохождение в активити метода onResume()
+    fun activity_IsResumed() {
+        TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
+
+    @Test // Проверка наличия элемента с id "searchEditText"
+    fun activity_SearchEditText_NotNull() {
+        scenario.onActivity {
+            val editText = it.findViewById<EditText>(R.id.searchEditText)
+            TestCase.assertNotNull(editText)
+        }
+    }
+
+    @Test // Проверка наличия элемента с id "toDetailsActivityButton"
+    fun activity_ToDetailsButton_NotNull() {
+        scenario.onActivity {
+            val toDetailsButton = it.findViewById<Button>(R.id.toDetailsActivityButton)
+            TestCase.assertNotNull(toDetailsButton)
+        }
+    }
+
+    @Test // Проверка наличия элемента с id "totalCountTextView"
+    fun activity_TotalCountTextView_NotNull() {
+        scenario.onActivity {
+            val totalCountTextView = it.findViewById<TextView>(R.id.totalCountTextView)
+            TestCase.assertNotNull(totalCountTextView)
+        }
+    }
+
+    @Test // Проверка наличия элемента с id "recyclerView"
+    fun activity_RecyclerView_NotNull() {
+        scenario.onActivity {
+            val recyclerView = it.findViewById<RecyclerView>(R.id.recyclerView)
+            TestCase.assertNotNull(recyclerView)
+        }
+    }
+
+    @Test // Проверка на невидимость элемента с id "totalCountTextView", перед запросом к API
+    fun activity_TotalCountTextView_IsInvisible() {
+        onView(withId(R.id.totalCountTextView)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
+    }
+
+    @Test // Проверка на видимость элемента с id "totalCountTextView", после выполнения запроса к API
+    fun activity_TotalCountTextView_IsVisible() {
+        onView(withId(R.id.searchEditText)).perform(click())
+            .perform(replaceText("algol"), closeSoftKeyboard())
+            .perform(pressImeActionButton())
+        onView(isRoot()).perform(delay())
+        onView(withId(R.id.totalCountTextView)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test // Проверка частичного отображения элемента с id "searchEditText"
+    fun activity_SearchEditText_isDisplayed() {
+        onView(withId(R.id.searchEditText)).check((matches(isDisplayed())))
+    }
+
+    @Test // Проверка отображения корректного текста с подсказкой на элементе с id "searchEditText"
+    fun activity_EditTextIsCorrectHintText() {
+        onView(withId(R.id.searchEditText)).check(matches(withHint("Enter keyword e.g. android")))
+    }
+
+    @Test //Проверка отображения корректного текста на кнопке с id "toDetailsActivityButton"
+    fun activity_ButtonToDetails_IsCorrectText() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(withText("to details")))
+    }
+
     @Test
     fun activitySearch_IsWorking() {
         onView(withId(R.id.searchEditText)).perform(click())
@@ -39,23 +118,42 @@ class MainActivityEspressoTest {
             //Внимание!
             //Перед тем как запускать тест, убедитесь в правильном количестве репозиториев. Скорее всего
             //оно будет меняться, несмотря на редкость языка (Algol).
-            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 3080")))
+            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 3082")))
         }
     }
 
+    @Test
+    fun activity_ButtonToDetails_AreEffectiveVisible() {
+        onView(withId(R.id.toDetailsActivityButton)).check(
+            matches(
+                withEffectiveVisibility(
+                    Visibility.VISIBLE
+                )
+            )
+        )
+    }
+
+    @Test
+    fun activity_ButtonToDetails_IsWorking() {
+        onView(withId(R.id.toDetailsActivityButton)).perform(click())
+        onView(withId(R.id.totalCountTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 0")))
+    }
+
+    // Функция для реализации задержки
     private fun delay(): ViewAction {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> = isRoot()
 
             override fun getDescription(): String = "wait for $2 seconds"
 
+            /** perform() занимается непосредственно Action’ом: нас интересует UiController, у
+            которого есть нужный нам метод. Мы “замораживаем” UI на 2 секунды. Этого
+            достаточно для того, чтобы асинхронный запрос через Retrofit отправился и вернулся с
+            ответом. Если у вас нестабильное или слабое соединение, можно увеличить время
+            ожидания, но имейте в виду, что время выполнения теста увеличится на
+            соответствующее время.*/
             override fun perform(uiController: UiController, view: View?) {
-                //perform() занимается непосредственно Action’ом: нас интересует UiController, у
-                //которого есть нужный нам метод. Мы “замораживаем” UI на 2 секунды. Этого
-                //достаточно для того, чтобы асинхронный запрос через Retrofit отправился и вернулся с
-                //ответом. Если у вас нестабильное или слабое соединение, можно увеличить время
-                //ожидания, но имейте в виду, что время выполнения теста увеличится на
-                //соответствующее время.
                 uiController.loopMainThreadForAtLeast(2000)
             }
         }
